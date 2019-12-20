36728fe89973b79ed99457faf5288863a27becd3
Michael Williams <manicdesigner@gmail.com>
2019-12-20 18:55:11 -0500

    built out drop table

--- migrations/20191220165708_cars.js
+++ migrations/20191220165708_cars.js
@@ -1,12 +1,21 @@
-
-exports.up = async function (knex) {
-  await knex.schema.createTable('cars', (table) => {
-    table.increments('id')
-    table.string('vin', 17).notNull().unique()
-
-  })
+exports.up = async function(knex) {
+  await knex.schema.createTable('cars', table => {
+    table.increments('id');
+    table
+      .string('vin', 17)
+      .notNull()
+      .unique();
+    table.string('make', 150).notNull();
+    table.text('model').notNull();
+    table
+      .integer('mileage')
+      .notNull()
+      .defaultTo(0);
+    table.string('transmission_type', 75).nullable();
+    table.string('title_status', 45).nullable();
+  });
 };
 
-exports.down = function (knex) {
-
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('cars');
 };
