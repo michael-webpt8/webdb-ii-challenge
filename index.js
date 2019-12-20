ea9537783f872d5d37f9eef3140e5a59a7f8d18a
Mikey Williams <manicdesigner@gmail.com>
2019-12-20 16:21:10 -0500

    index built out

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require('helmet');
+const carsRouter = require('./router/carsRouter');
+
+const server = express();
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 5000;
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/cars', carsRouter);
+
+server.use((req, res) => {
+    res.status(404).json({ message: "404 page not found." })
+})
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ message: "Something went wrong" })
+})
