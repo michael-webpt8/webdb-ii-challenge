fdedc0aa0a6e4b0361c59d75a4ac06dfc0f7a477
Michael Williams <manicdesigner@gmail.com>
2019-12-21 15:55:18 -0500

    delete added

--- router/carsRouter.js
+++ router/carsRouter.js
@@ -12,7 +12,17 @@ const router = express.Router();
  */
 router.get('/', async (req, res) => {
   try {
-    const cars = await db('cars').select();
+    const cars = await db
+      .select(
+        'id',
+        'vin',
+        'make',
+        'model',
+        'mileage',
+        'transmission_type',
+        'title_status'
+      )
+      .from('cars');
     res.json(cars);
   } catch (err) {
     next(err);
@@ -28,7 +38,15 @@ router.get('/', async (req, res) => {
  */
 router.post('/', async (req, res, next) => {
   try {
-    const ids = await db('cars').insert(req.body);
+    // const [ids] = await db('cars').insert(req.body);
+    const ids = await db('cars').insert({
+      vin: req.body.vin,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      transmission_type: req.body.transmission_type || null,
+      title_status: req.body.title_status || null
+    });
     const newCar = await db('cars')
       .where({ id: ids[0] })
       .first();
@@ -48,12 +66,51 @@ router.post('/', async (req, res, next) => {
  */
 router.put('/:id', async (req, res, next) => {
   try {
-    const carId = await db('cars').insert(req.body);
-    const newCar = await db('cars')
-      .where({ id: carId[0] })
-      .first();
+    const payload = {
+      vin: req.body.vin,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      transmission_type: req.body.transmission_type || null,
+      title_status: req.body.title_status || null
+    };
 
-    res.json(201).json(newCar);
+    await db('cars')
+      .where({ id: req.params.id })
+      .update(payload);
+
+    res.status(201).json(
+      await db('cars')
+        .where({ id: req.params.id })
+        .first()
+
+      // WHY DOESNT THIS DESTRUCTURE WORK?
+      // const [carId] = await db('cars') <-- carId destructure into line 94.
+      //   .where( id: req.params.id )
+      //   .update(payload);
+
+      // res.status(201).json(
+      //   await db('cars')
+      //     .where('id', carId)
+      //     .first()
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * DELETE
+ * Endpoint: `/cars/:id`
+ * description: delete selected item from cars
+ */
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await db('cars')
+      .where('id', req.params.id)
+      .del();
+
+    res.json(204);
   } catch (err) {
     next(err);
   }
