4ed94a5049134731cb275889c8ed63a3e876468d
Michael Williams <manicdesigner@gmail.com>
2019-12-20 21:38:28 -0500

    add port with host

--- index.js
+++ index.js
@@ -11,15 +11,11 @@ server.use(express.json());
 
 server.use('/cars', carsRouter);
 
-server.use((req, res) => {
-    res.status(404).json({ message: "404 page not found." })
-})
-
 server.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).json({ message: "Something went wrong" })
-})
+  console.log(err);
+  res.status(500).json({ message: 'Something went wrong' });
+});
 
 server.listen(port, host, () => {
-    console.log(`\n Running on http://${host}:${port}\n`)
-})
+  console.log(`\n Running on http://${host}:${port}\n`);
+});
