741833610bcfabb92581318e3046779c3c346f30
Michael Williams <manicdesigner@gmail.com>
2019-12-20 22:01:50 -0500

    add post

--- router/carsRouter.js
+++ router/carsRouter.js
@@ -13,4 +13,17 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res, next) => {
+  try {
+    const ids = await db('cars').insert(req.body);
+    const newCar = await db('cars')
+      .where({ id: ids[0] })
+      .first();
+
+    res.status(201).json(newCar);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
