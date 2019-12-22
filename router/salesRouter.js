8444204a36e40b1112a1a5461ce168959c60e2d2
Michael Williams <manicdesigner@gmail.com>
2019-12-21 23:55:39 -0500

    exports module added

--- router/salesRouter.js
+++ router/salesRouter.js
@@ -3,3 +3,17 @@ const helmet = require('helmet');
 const db = require('../utils/db');
 
 const router = express.Router();
+
+router.get('/:id/sales', async (req, res, next) => {
+  try {
+    res.json(
+      await db('sales')
+        .where({ id: req.params.id })
+        .select('sold', 'sold_to', 'sold_by', 'list_price', 'sold_price')
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
