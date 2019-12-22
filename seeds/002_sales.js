ccbc43cdcc1b4ad9b3b82a18306f9063d2d8977d
Michael Williams <manicdesigner@gmail.com>
2019-12-22 18:17:02 -0500

    seed data entered and updated

--- seeds/002_sales.js
+++ seeds/002_sales.js
@@ -6,26 +6,26 @@ exports.seed = async function(knex) {
       sold_to: 'Mikey Williams',
       sold_by: 'Leslie Bergeron',
       list_price: 500.45,
-      sold_price: 455.65
+      sold_price: 455.65,
     },
     {
       sold: false,
       sold_to: 'jerry finn',
-      list_price: 535.45
+      list_price: 535.45,
     },
     {
       sold: true,
       sold_to: 'happy wilson',
       sold_by: 'carol Bergeron',
       list_price: 540.76,
-      sold_price: 455.65
+      sold_price: 455.65,
     },
     {
       sold: false,
       sold_to: 'Mikey Williams',
       sold_by: 'Leslie Bergeron',
       list_price: 500.45,
-      sold_price: 455.65
-    }
+      sold_price: 455.65,
+    },
   ]);
 };
