2e904edc3613e1f9c20691e5912104f884554e63
Michael Williams <manicdesigner@gmail.com>
2019-12-22 08:16:19 -0500

    add sales table schema add seed data

--- migrations/20191221211814_sales.js
+++ migrations/20191221211814_sales.js
@@ -1,10 +1,14 @@
 exports.up = async function(knex) {
   return knex.schema.createTable('sales', table => {
-    table.increments('sales_id');
-    table.boolean('sold').nullable();
-    table.string('sold_to', 150).notNullable();
-    table.string('sold_by', 150).notNullable();
-    table.decimal('list_price').nullable();
+    table.increments('id');
+    table
+      .integer('sales_id')
+      .unique()
+      .notNullable();
+    table.boolean('sold').notNullable();
+    table.string('sold_to', 150).nullable();
+    table.string('sold_by', 150).nullable();
+    table.decimal('list_price').notNullable();
     table.decimal('sold_price').nullable();
     table
       .foreign('sales_id')
--- seeds/002_sales.js
+++ seeds/002_sales.js
@@ -1,13 +1,35 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function(knex) {
+  await knex('sales').truncate();
+  await knex('sales').insert([
+    {
+      sales_id: 1,
+      sold: true,
+      sold_to: 'Mikey Williams',
+      sold_by: 'Leslie Bergeron',
+      list_price: 500.45,
+      sold_price: 455.65
+    },
+    {
+      sales_id: 2,
+      sold: false,
+      sold_to: 'jerry finn',
+      list_price: 535.45
+    },
+    {
+      sales_id: 3,
+      sold: true,
+      sold_to: 'happy wilson',
+      sold_by: 'carol Bergeron',
+      list_price: 540.76,
+      sold_price: 455.65
+    },
+    {
+      sales_id: 4,
+      sold: false,
+      sold_to: 'Mikey Williams',
+      sold_by: 'Leslie Bergeron',
+      list_price: 500.45,
+      sold_price: 455.65
+    }
+  ]);
 };
