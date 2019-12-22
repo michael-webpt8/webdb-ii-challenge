c9489140adb1b3394318852202d27dd972fb1201
Michael Williams <manicdesigner@gmail.com>
2019-12-22 00:18:39 -0500

    const for adding selects and less error prone

--- router/salesRouter.js
+++ router/salesRouter.js
@@ -2,6 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 const db = require('../utils/db');
 
+const SOLD = 'sold';
+const SOLD_TO = 'sold_to';
+const SOLD_BY = 'sold_by';
+const LIST_PRICE = 'list_price';
+const SOLD_PRICE = 'sold_price';
+
 const router = express.Router();
 
 /**
@@ -14,7 +20,35 @@ router.get('/:id/sales', async (req, res, next) => {
     res.json(
       await db('sales')
         .where({ id: req.params.id })
-        .select('sold', 'sold_to', 'sold_by', 'list_price', 'sold_price')
+        .select(SOLD, SOLD_TO, SOLD_BY, LIST_PRICE, SOLD_PRICE)
+    );
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * POST / CREATE
+ * Endpoint: `/cars/:id/sales`
+ */
+router.post('/:id/sales', async (req, res, next) => {
+  try {
+    const payload = {
+      sold: req.body.sold || null,
+      sold_to: req.body.sold_to,
+      sold_by: req.body.sold_by,
+      list_price: req.body.list_price || null,
+      sold_price: req.body.sold_price || null
+    };
+
+    const [id] = await db('sales')
+      .where({ id: req.params.id })
+      .insert(payload);
+    res.status(201).json(
+      await db('sales')
+        .where({ id: id })
+        .select(SOLD, SOLD_TO, SOLD_BY, LIST_PRICE, SOLD_PRICE)
+        .first()
     );
   } catch (err) {
     next(err);
