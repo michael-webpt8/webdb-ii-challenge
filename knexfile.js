8a5045da196a7b4f874f21d42228eb195663b0db
Michael Williams <manicdesigner@gmail.com>
2019-12-23 12:10:26 -0500

    add FK contraint ON

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,19 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: "sqlite3", // DBMS driver
+    client: 'sqlite3', // DBMS driver
     useNullAsDefault: true, // needed when using sqlite
-    connection: { // location of our DB
-      filename: "./data/car-dealer.db3"
-    }
+    connection: {
+      // location of our DB
+      filename: './data/car-dealer.db3',
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
-
 };
